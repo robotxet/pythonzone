1b9026cd9cbe96022cccfd84437c4119efe0ae31
rzet <robot.xet@gmail.com>
2014-07-26 13:36:19 +0400

    packaging django tutorial

---
+++ django-polls/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+	name='django-polls',
+	version='0.1',
+	packages=['polls'],
+	include_package_data=True,
+	license='MIT License',
+	description='A simple Django app to conduct web-based polls.',
+	long_description=README,
+	url='http://www.example.com',
+	author='Yaroslav Romanov',
+	author_email='robot.xet@gmail.com',
+	classifiers=[
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License', # example license
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+	]
+)
