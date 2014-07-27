702198fb998b2878bc49ea68ad47b760282fa99d
rzet <robot.xet@gmail.com>
2014-07-27 13:54:08 +0400

    added bubble sort realisation

---
+++ solutions/sorting/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(x):
+    length = len(x)
+    for i in range(1, length):
+        sorted = False
+        for j in range(1, length - i):
+            if x[j] > x[j + 1]:
+                x[j], x[j + 1] = x[j + 1], x[j]
+                sorted = True
+        if not sorted:
+            break
