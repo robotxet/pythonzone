3c771a1a2bb27f63be4d2dc9287c82ce4f94b47d
rzet <robot.xet@gmail.com>
2014-07-27 12:37:23 +0400

    added solution for tile cost to cover WxH floor

---
+++ algorithms/numbers/find_cost_to_cover_floor.py
@@ -0,0 +1,16 @@
+"""
+Find Cost of Tile to Cover W x H Floor:
+Calculate the total cost of tile if would take to cover a floor plan of
+width and height, using a cost entered by the user.
+"""
+
+width = float(raw_input("Width of floor: "))
+length = float(raw_input("Length of floor: "))
+cost = float(raw_input("Cost of tile: "))
+
+tile_width = float(raw_input("Width of tile: "))
+tile_height = float(raw_input("Height of tile: "))
+
+total_tile_number = (round(width / tile_width)) * (round(length / tile_height))
+
+print "Total tile cost for %sx%s floor is %s" % (width, length, total_tile_number * cost)
