38ee7fb2ff107eff5b3d5343a671d9599965f19f
rzet <robot.xet@gmail.com>
2014-07-27 14:24:17 +0400

    added insertion sort

---
+++ solutions/sorting/insertion_sort.py
@@ -0,0 +1,7 @@
+def insertion_sort(x):
+    if len(x) < 2:
+        return
+    for i in range(1, len(x)):
+        for j in range(i, 0, -1):
+            if x[j] < x[j - 1]:
+                x[j], x[j - 1] = x[j - 1], x[j]
