25d436dcdb5cc1026b968a9db861b79b8bd317ba
rzet <robot.xet@gmail.com>
2014-07-27 14:17:38 +0400

    added coctail sort (shaker sort)

---
+++ solutions/sorting/cocktail_sort.py
@@ -0,0 +1,20 @@
+def coctail_sort(x):
+    swapped = True
+    length = len(x) - 1
+    begin = -1
+    end = length
+    while swapped:
+        swapped = False
+        begin += 1
+        for i in range(begin, end):
+            if x[i] > x[i + 1]:
+                x[i], x[i + 1] = x[i + 1], x[i]
+                swapped = True
+        if not swapped:
+            break
+        swapped = False
+        end -= 1
+        for i in range(end, begin, -1):
+            if x[i] < x[i - 1]:
+                x[i], x[i - 1] = x[i - 1], x[i]
+                swapped = True
