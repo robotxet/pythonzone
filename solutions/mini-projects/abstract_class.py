c958042fcf6d69b024fc92d6b06f99efa8d27aa0
rzet <robot.xet@gmail.com>
2014-08-08 20:12:59 +0400

    added real abstract class using abc module

--- solutions/mini-projects/abstract_class.py
+++ solutions/mini-projects/abstract_class.py
@@ -5,17 +5,39 @@ SavingsAccount and BusinessAccount.
 
 For shortening i simply crate one class which inherits from Account
 """
+from abc import *
 
 
 class Account(object):
+    # this is fake abstraction
     def __init__(self, id):
         self.id = id
+
+
+class RealAbstractAccout(metaclass=ABCMeta):
+
+    @abstractproperty
+    def id_number(self):
+        return 0
 
 
 class CheckingAccount(Account):
     def __init__(self, id, name):
         Account.__init__(self, id)
         self.name = name
+
+    def __str__(self):
+        return "%s, %s" % (self.id, self.name)
+
+
+class RealCheckingAccount(RealAbstractAccout):
+    def __init__(self, id, name):
+        Account.__init__(self, id)
+        self.name = name
+
+    @property
+    def id_number(self):
+        return self.id
 
     def __str__(self):
         return "%s, %s" % (self.id, self.name)
